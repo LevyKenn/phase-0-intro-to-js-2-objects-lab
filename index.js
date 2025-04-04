a61f12d79386e6ebeb653d46628f7b8cbb753638
Emmah Gicheru <gicheruemmah@gmail.com>
2025-04-04 23:28:28 +0300

    Complete object manipulation functions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const employee = {
     name: "Simon",
     streetAddress: "Mwiki road",
@@ -22,3 +23,37 @@ function destructivelyDeleteFromEmployeeByKey(employee, key){
     delete employee[key];
     return employee;
 }
+=======
+// Employee object
+const employee = {
+  name: "Llewellyn Muirwe",
+  streetAddress: "123 Main St"
+};
+
+// 1. Non-destructive update
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+
+// 2. Destructive update
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// 3. Non-destructive delete
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// 4. Destructive delete
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+>>>>>>> 33645a7 (Complete object manipulation functions)
